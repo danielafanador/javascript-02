bdd7de3537b0e914e7294e2e280d5741b3ec7414
Daniel Afanador <danielthemisfits@gmail.com>
2021-10-05 14:38:34 -0300

    Anotaciones sobre los tipos de función

---
+++ funciones.js
@@ -0,0 +1,15 @@
+// Funciones declarativas
+
+function myFunction () {
+    return 3;
+}
+
+// Funciones de expresión
+
+var myFunction = function () {return a + b}
+
+// Ejemplo con otra nomenclatura
+
+function saludarEstudiantes () {
+    console.log (`Hola, ${estudiante}`);
+}
